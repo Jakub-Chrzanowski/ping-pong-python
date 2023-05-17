99d0e06fa2b4d3a8a29d1fc04b58b8a2ca914453
Jakub-Chrzanowski <132926284+Jakub-Chrzanowski@users.noreply.github.com>
2023-05-17 20:20:59 +0200

    Dodanie tabeli z wynikami

--- gra.py
+++ gra.py
@@ -8,8 +8,15 @@ ball_y = HEIGHT // 2
 paddle1_y = HEIGHT // 2 - 2
 paddle2_y = HEIGHT // 2 - 2
 
+score1 = 0
+score2 = 0
 def draw_board():
     os.system("clear" if os.name == "posix" else "cls")
+    print("\033[1m" + " " * (WIDTH + 6) + "PING PONG" + "\033[0m")
+    print("\033[1m" + " " * (WIDTH + 2) + "Score" + "\033[0m")
+    print("\033[1m" + " " * (WIDTH + 2) + f"Player 1: {score1}" + "\033[0m")
+    print("\033[1m" + " " * (WIDTH + 2) + f"Player 2: {score2}" + "\033[0m")
+    print("\033[1m" + "+" + "-" * WIDTH + "+" + "\033[0m")
     for row in range(HEIGHT):
         print("\033[1m" + "|" + "\033[0m", end="")
         for col in range(WIDTH):
@@ -36,3 +43,10 @@ def move_paddles():
     if keyboard.is_pressed('k') and paddle2_y + 2 < HEIGHT:
         paddle2_y += 1
 
+def play_pong():
+        
+        move_paddles()
+        ball_x += ball_speed_x
+        ball_y += ball_speed_y
+        
+        draw_board()
